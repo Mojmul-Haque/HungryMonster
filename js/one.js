fab45a7688552575adbfbc9017255354afa9785e
Mojmul Haque <mojmul150@gmail.com>
2021-02-07 13:42:57 +0600

    search name done

--- js/one.js
+++ js/one.js
@@ -13,17 +13,49 @@ document.getElementById('search_meal').addEventListener('click', () => {
 //display recipe by search name
 const displayMeals = data => {
     console.log(data)
-    const catchMeal = data.meals.map(element => {
-        const mealName = element.strMeal;
-        const mealPic = element.strMealThumb
-        console.log(mealName, mealPic)
+        // const catchMeal = data.meals.map(element => {
+        //     const mealName = element.strMeal;
+        //     const mealPic = element.strMealThumb
+        //     console.log(mealName, mealPic)
 
-        // now show me the item in display
-        const ul = document.getElementById('show_recipe');
-        const recipeInfo = ` <li>${mealName}</li>
+    //     // now show me the item in display
+    //     const ul = document.getElementById('show_recipe');
+    //     const recipeInfo = ` <li>${mealName}</li>
+    //     `
+    //     ul.innerHTML = recipeInfo
+
+
+    // })
+    const allmealsList = data.meals
+    const recipeContainer = document.getElementById('show_recipe');
+    for (let i = 0; i < allmealsList.length; i++) {
+        const mealName = allmealsList[i].strMeal
+        const mealPic = allmealsList[i].strMealThumb
+        console.log(mealName)
+
+        const recipeInnerDIv = document.createElement('div')
+        recipeInnerDIv.className = 'main_recipe'
+        const recipeInfo = `  <img src ="${mealPic}">
+        <h4>${mealName}</h4>
+       
         `
-        ul.innerHTML = recipeInfo
+        recipeInnerDIv.innerHTML = recipeInfo
+
+        recipeContainer.appendChild(recipeInnerDIv)
 
 
-    })
+        // recipe details 
+
+        recipeInnerDIv.addEventListener('click', () => {
+            console.log('click')
+        })
+    }
+
+
+
+
+
+
+
+
 }

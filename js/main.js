69248a28569c3237c978b8fab841ccd2a9573f40
Mojmul Haque <mojmul150@gmail.com>
2021-02-07 23:24:05 +0600

    done all

--- js/main.js
+++ js/main.js
@@ -8,84 +8,79 @@ document.getElementById('search_meal').addEventListener('click', () => {
     } else {
         alert('give a value in input field')
     }
-
-
-
 })
 
-//display recipe by search name
+
+//show display recipe  by search name
 const displayMeals = data => {
-    console.log(data)
     if (data.meals !== null) {
-        let inputMeal = document.getElementById('input_meal').value;
 
         const recipeContainer = document.getElementById('show_recipe');
         const allmealsList = data.meals
         allmealsList.forEach(element => {
 
+                const mealName = element.strMeal
+                const mealPic = element.strMealThumb
 
+                const recipeInnerDIv = document.createElement('div')
+                recipeInnerDIv.className = 'col-lg-3 recipie'
 
-            const mealName = element.strMeal
-            const mealPic = element.strMealThumb
-
-            const recipeInnerDIv = document.createElement('div')
-            recipeInnerDIv.className = 'col-lg-3 recipie'
-
-            const recipeInfo = `     <img src ="${mealPic}">
+                const recipeInfo = `     <img src ="${mealPic}">
             <h4>${mealName}</h4>     `
 
-            recipeInnerDIv.innerHTML = recipeInfo
+                recipeInnerDIv.innerHTML = recipeInfo
 
-            recipeContainer.appendChild(recipeInnerDIv)
+                recipeContainer.appendChild(recipeInnerDIv)
 
+                // recipe details event handelar
 
+                recipeInnerDIv.addEventListener('click', () => {
 
-            // recipe details event handelar
+                    const recipeDetails = document.getElementById('meals_details')
 
-            recipeInnerDIv.addEventListener('click', () => {
+                    var recipeDetailsContainer = document.createElement('div');
+                    recipeDetailsContainer.className = 'col-lg-4'
 
-                const recipeDetails = document.getElementById('meals_details')
-
-                var recipeDetailsContainer = document.createElement('div');
-                recipeDetailsContainer.className = 'col-lg-4'
-
-                const recipeDetailsInfo = `<img src = "${mealPic}">
+                    const recipeDetailsInfo = `<img src = "${mealPic}">
                     <h3>${mealName}</h3> 
                     <h4>Ingredients</h4>
                     `
+                    recipeDetailsContainer.innerHTML = recipeDetailsInfo;
+                    recipeDetails.appendChild(recipeDetailsContainer)
+                        // ingridents element;
+                    const ingredients = [element.strIngredient1, element.strIngredient2, element.strIngredient3, element.strIngredient4, element.strIngredient5, element.strIngredient6, element.strIngredient7, element.strIngredient8, element.strIngredient9, element.strIngredient10, element.strIngredient11, element.strIngredient12, element.strIngredient13, element.strIngredient14, element.strIngredient15, element.strIngredient16, element.strIngredient17, element.strIngredient18, element.strIngredient19, element.strIngredient20]
 
-                recipeDetailsContainer.innerHTML = recipeDetailsInfo;
-                recipeDetails.appendChild(recipeDetailsContainer)
-                    // ingridents element;
-                const ingredients = [element.strIngredient1, element.strIngredient2, element.strIngredient3, element.strIngredient4, element.strIngredient5, element.strIngredient6, element.strIngredient7, element.strIngredient8, element.strIngredient9, element.strIngredient10, element.strIngredient11, element.strIngredient12, element.strIngredient13, element.strIngredient14, element.strIngredient15, element.strIngredient16, element.strIngredient17, element.strIngredient18, element.strIngredient19, element.strIngredient20]
+                    ingredients.map(item => {
+                        if (item !== '') {
+                            const ingredientDiv = document.createElement('div')
+                            ingredientDiv.className = 'ingredients_item'
 
-                ingredients.map(item => {
-                    if (item !== '') {
-                        const ingredientDiv = document.createElement('div')
-                        ingredientDiv.className = 'ingredients_item'
+                            const allIngridents = `<p> <i class="fas fa-check-square"></i> ${item}</p>`
+                            ingredientDiv.innerHTML = allIngridents;
 
-                        const allIngridents = `<p> <i class="fas fa-check-square"></i> ${item}</p>`
-                        ingredientDiv.innerHTML = allIngridents;
+                            recipeDetailsContainer.appendChild(ingredientDiv)
 
-                        recipeDetailsContainer.appendChild(ingredientDiv)
-
-                    }
+                        }
+                    })
 
                 })
-
-
             })
-
-
-
-
-        })
+            //eror controler
+        conditionShow('eror_msg', 'none', 'eror_handaler', 'block')
 
     } else {
         const eror = document.getElementById('eror_msg')
-        const erorInfo = `<h3>No meals found</h3>
-        `
+        const erorInfo = `<h3>No meals found</h3> `
         eror.innerHTML = erorInfo
+        conditionShow('eror_msg', 'block', 'eror_handaler', 'none')
     }
 
+}
+
+// eror condition
+conditionShow = (erorId, istrue, notErorId, isFalse) => {
+    const eror = document.getElementById(erorId)
+    eror.style.display = istrue
+    const notEror = document.getElementById(notErorId)
+    notEror.style.display = isFalse
 }
